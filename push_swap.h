faddef3c1f22d4ba7b8bd428a753f27009ddd44f
Nathan GOBERT <ngobert@e2r11p21.clusters.42paris.fr>
2022-01-24 15:41:33 +0100

    Added protos to header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 11:13:30 by ngobert           #+#    #+#             */
-/*   Updated: 2022/01/21 14:06:29 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/01/24 15:40:14 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,17 @@
 #include <limits.h>
 
 void	ft_init_a(int argc, char **argv, t_list **head_a);
-int	ft_is_a_sorted(t_list **head_a);
+int		ft_is_a_sorted(t_list **head_a);
+int		sa(t_list **head_a);
+int		sb(t_list **head_b);
+void	ss(t_list **head_a, t_list **head_b);
+int		ra(t_list **head);
+int		rb(t_list **head);
+void	rr(t_list **head_a, t_list **head_b);
+void	rra(t_list **head);
+void	rrb(t_list **head);
+void	rrr(t_list **head_a, t_list **head_b);
+int		pa(t_list **head_a, t_list **head_b);
+int		pb(t_list **head_a, t_list **head_b);
 
 #endif
