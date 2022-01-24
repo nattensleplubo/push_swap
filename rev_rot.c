70f424f02386c9204f8d7cbd40e7773114ca901d
Nathan GOBERT <ngobert@e2r11p21.clusters.42paris.fr>
2022-01-24 13:29:30 +0100

    Added reverse rotations

--- rev_rot.c
+++ rev_rot.c
@@ -6,15 +6,56 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 09:53:16 by ngobert           #+#    #+#             */
-/*   Updated: 2022/01/24 09:58:17 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/01/24 13:23:57 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	rra(t_list **head)
+void	rra(t_list **head)
 {
-	if (ft_lstsize(*head) == 1)
-		return (0);
+	int	i;
+	int	size;
+	t_list	*tail;
+	t_list	*temp;
 	
+	i = 0;
+	size = ft_lstsize(*head);
+	tail = *head;
+	temp = ft_lstlast(*head);
+	while (i < size - 1)
+	{
+		tail = tail->next;
+		i++;
+	}
+	tail->next = NULL;
+	temp->next = *head;
+	*head = temp;
+}
+
+void	rrb(t_list **head)
+{
+	int	i;
+	int	size;
+	t_list	*tail;
+	t_list	*temp;
+	
+	i = 0;
+	size = ft_lstsize(*head);
+	tail = *head;
+	temp = ft_lstlast(*head);
+	while (i < size - 1)
+	{
+		tail = tail->next;
+		i++;
+	}
+	tail->next = NULL;
+	temp->next = *head;
+	*head = temp;
+}
+
+void	rrr(t_list **head_a, t_list **head_b)
+{
+	rra(head_a);
+	rrb(head_b);
 }
