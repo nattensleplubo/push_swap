ff9fdc40baf1653b1773d340ef982fb4b1ea9fc9
Nathan GOBERT <ngobert@e2r10p6.clusters.42paris.fr>
2022-01-25 16:39:02 +0100

    Began to sort 5

--- ft_sort_low.c
+++ ft_sort_low.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 10:24:18 by ngobert           #+#    #+#             */
-/*   Updated: 2022/01/25 14:49:52 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/01/25 16:30:50 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,11 @@ void	ft_sort_three(t_list **head)
 		sa(head);
 }
 
-// 	one	two	three
-// 	3	2	1		DONE
-// 	1	3	2		DONE
-// 	2	3	1		DONE
-// 	3	1	2		DONE
-// 	2	1	3		DONE
+void	ft_sort_small(t_list **head_a, t_list **head_b)
+{
+	int	i;
+	int	size;
+
+	size = ft_lstsize(*head_a);
+	
+}
